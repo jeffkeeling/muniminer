c4f3937a2e97594c0f10afd5bd7f20cc5c812901
Jeff Keeling <jmk212@gmail.com>
2015-04-26 22:11:56 -0400

    Substantial opitimization of css/js
    -added custom profile name back into request form

--- static/js.js
+++ static/js.js
@@ -1,11 +1,72 @@
-$(document).ready(function(){
+(function($, window, document) {
+    $(function() {
+        //event listeners
+        $('#pdf-container').on('dragover', dragOver).
+            on('dragleave', dragLeave).
+            on('drop', dropFile);
+        $('#pdf-input').on('change', pdfChanged);
+        $('#profile-dropdown').on('change', profileDropdownChanged);
+        $('#custom-profile-name').on('keyup change', customNameChanged);
+        $('#custom-profile-regex').on('keyup change', customRegexChanged);
+        $("form").submit(submitForm);
+        $("#save-profiles").on('click', saveProfiles);
+        $('#add-new-profile').on('click', addNewProfile);
 
-    //populate dropdown with custom profiles
-    for (var key in localStorage){
-       $('.profile-dropdown option').first().after(
-            "<option data-custom='true' value='" + key + "'>" + key + "</option>"
-        );
+        if(window.location.pathname === '/') {
+            //populate dropdown with custom profiles
+            var profileHolder = '',
+                key;
+
+            for (key in localStorage){
+                profileHolder +=  $('#base-container option').clone().
+                    attr('value', key).html(key).wrap('<div/>').parent().html();
+            }
+
+            //place custom profiles after default
+            if (profileHolder.length > 0) {
+                $('#profile-dropdown option').first().after(profileHolder);
+            }
+        }
+
+        if(window.location.pathname === '/edit-profiles') {
+            var $listContainer = $('#edit-profile-list'),
+                profileHolder = '',
+                key;
+
+            for (key in localStorage) {
+                profileHolder += $('#base-container li').clone().
+                    find('input').attr({'value': key, 'data-originalkey': key}).end().
+                    find('textarea').html(localStorage[key]).end().
+                    find('.delete-profile').attr('data-originalkey', key).end().
+                    wrap('<div/>').parent().html();
+            }
+            if (profileHolder.length > 0) {
+                $listContainer.append(profileHolder);
+            }
+
+            $listContainer.on('click', '.delete-profile', deleteProfile).
+                on('keyup change', 'input', editProfileChanged).
+                on('keyup change', 'textarea', editProfileChanged);
+        }
+    });
+
+    var formObj = {
+        pdf: false,
+        profileDropdown: false,
+        profileName: false,
+        profileRegex: false
     }
+
+    var formObjChanged = function() {
+        $('.error').hide();
+        if ( formObj.pdf && (formObj.profileDropdown || (formObj.profileName && formObj.profileRegex)) ){
+            $('#submit-form').attr("disabled", false).removeClass('disabled-button');
+        } else {
+            $('#submit-form').attr("disabled", true).addClass('disabled-button');
+        }
+    }
+
+    $(formObj).on('change', formObjChanged);
 
     var dragOver = function(e) {
         e.preventDefault();
@@ -23,68 +84,111 @@ $(document).ready(function(){
         e.preventDefault();
         e.stopPropagation();
         var files = e.originalEvent.dataTransfer.files,
-            mimeType= files[0].type; 
+            mimeType= files[0].type;
+
         $('#pdf-container').removeClass('hovered');
     
         if (mimeType !== 'application/pdf') {
+            $('#pdf-error').html("Provided file not a .pdf document").show();
             return;
         }
     
-        $('input[name="pdfFile"]').prop('files', files);
+        $('#pdf-input').prop('files', files);
     };
 
-    var checkPdfPresent = function(e) {
-        if ( $('input[name="pdfFile"]').val() ){
-            $('#pdf-container > p:first-child').addClass('valid');
-            return true;
+    var pdfChanged = function(e) {
+        if ( $(e.currentTarget).val() ){
+            $('#pdf-instruction').addClass('valid');
+            formObj.pdf = true;
         } else {
-            $('#pdf-container > p:first-child').removeClass('valid');
-            return false;
+            $('#pdf-instruction').removeClass('valid');
+            formObj.pdf = false;
         }
+        $(formObj).trigger('change');
     };
 
-    var checkProfilePresent = function(e) {
-        if($('.profile-dropdown').val() !== 'default' || 
-                ($('.customProfileRegex').val() && $('input[name="customProfile"]').val()) ){
-            $('#profile-container > p:first-child').addClass('valid');
-            return true;
+    var profileDropdownChanged = function(e) {
+        if ($(e.currentTarget).val() !== 'default') {
+            $('#profile-instruction').addClass('valid');
+            $('#custom-profile-regex').val('')
+            $('#custom-profile-name').val('');
+            formObj.profileDropdown = true;
+            formObj.profileName = false;
+            formObj.profileRegex = false;
         } else {
-            $('#profile-container > p:first-child').removeClass('valid');
-            return false;
+            $('#profile-instruction').removeClass('valid');
+            $('#custom-profile-regex').val('')
+            $('#custom-profile-name').val('');
+            formObj.profileDropdown = false;
+            formObj.profileName = false;
+            formObj.profileRegex = false;
         }
+        $(formObj).trigger('change');
     };
 
-    var checkForm = function(e) {
-        checkProfilePresent();
-        if ( checkPdfPresent() && checkProfilePresent() ){
-            $('input[type="submit"]').attr("disabled", false).removeClass('disabledButton');
+    var customNameChanged = function (e) {
+        if ( $(e.currentTarget).val() ) {
+            $('#profile-dropdown').val('default');
+            formObj.profileDropdown = false;
+            formObj.profileName = true;
         } else {
-            $('input[type="submit"]').attr("disabled", true).addClass('disabledButton');
+            formObj.profileName = false;
         }
+        customProfileChanged();
     };
+    var customRegexChanged = function (e) {
+        if ( $(e.currentTarget).val() ){
+            $('#profile-dropdown').val('default');
+            formObj.profileDropdown = false;
+            formObj.profileRegex = true;
+        } else {
+            formObj.profileRegex = false;
+        }
+        customProfileChanged()
+    };
+
+    var customProfileChanged = function () {
+        if (formObj.profileName && formObj.profileRegex) {
+            $('#profile-instruction').addClass('valid');
+        } else {
+            $('#profile-instruction').removeClass('valid');
+        }
+        $(formObj).trigger('change');
+    };    
 
     var submitForm = function(e) {
         e.preventDefault();
-        var file = $('input[name="pdfFile"]').val(),
-            data = new FormData();
+        var data = new FormData(),
+            $profileDropdown;
         
-        data.append('myfile', $('input[name="pdfFile"]')[0].files[0]);
+        data.append('myfile', $('#pdf-input')[0].files[0]);
         
-        if ($('.profile-dropdown').val() !== 'default') {
-            if($('.profile-dropdown').find(':selected').data('custom')) {
-                var localStorageRegex = localStorage.getItem($('.profile-dropdown').val() );
+        if (formObj.profileDropdown) {
+            $profileDropdown = $('#profile-dropdown')
+            if( $profileDropdown.find(':selected').data('custom') ) {
+                var localStorageRegex = localStorage.getItem($profileDropdown.val() );
                 data.append('profileRegex', localStorageRegex);
             } else {
-                data.append('profileName', $('.profile-dropdown').val());
+                data.append('profileName', $profileDropdown.val());
             }
-            
         } else {
-            var nameVal = $('input[name="customProfile"]').val();
-            var regexVal = $('.customProfileRegex').val()
-            data.append('profileRegex', regexVal);
+            var nameVal = $('#custom-profile-name').val(),
+                regexVal = $('#custom-profile-regex').val(),
+                key;
 
+            for (key in localStorage) {
+                if (key === nameVal) {
+                    $('#profile-error').html("Profile name already in use").show();
+                    $('#profile-instruction').removeClass('valid');
+                    $('#submit-form').attr("disabled", true).addClass('disabled-button');
+                    return;
+                }
+            }
+            data.append('profileName', regexVal);
+            data.append('profileRegex', regexVal);
             localStorage.setItem(nameVal, regexVal);
         }
+
         $.ajax({
             url: '/upload',
             type: 'POST',
@@ -94,101 +198,73 @@ $(document).ready(function(){
             contentType: false, // Set content type to false as jQuery will tell the server its a query string request
             success: function(data, textStatus, jqXHR)
             {
-                $('form').remove();
-                $('nav ul').prepend('<li><a class="" href="/">&#171; New PDF</a></li>');
-                $('header').after(
-                    "<div>" + data + "</div"
-                );
+                $('#form-container').html(data);
             },
             error: function(jqXHR, textStatus, errorThrown)
             {
-                console.log(jqXHR);
-                console.log('ERRORS: ' + textStatus);
+                 $('#pdf-error').html("There was an error: " + textStatus).show();
             }
         });
     };
 
-    var saveProfiles = function() {
-        localStorage.clear();
-        $('input.profile-name').each(function(index,data) {
-           var nameVal = $(this).val();
-           var regexVal =  $(this).parent().siblings('div').find('textarea').val();
-           localStorage.setItem(nameVal, regexVal);
+    /*
+    Edit Profiles Page
+    */
+    var editProfileChanged = function(e) {
+        $('#edit-profiles-success').hide();
+
+        var emptyFields = false;
+        $('#edit-profile-list input').each(function() {
+            if ( $(this).val() === '' ){
+                emptyFields = true;
+            }
         });
-        
+        $('#edit-profile-list textarea').each(function() {
+            if ( $(this).val() === '' ){
+                emptyFields = true;
+            }
+        });
+
+        if(!emptyFields) {
+            $('#save-profiles').attr("disabled", false).removeClass('disabled-button');
+        } else {
+            $('#save-profiles').attr("disabled", true).addClass('disabled-button');
+        }
+    };
+
+    var addNewProfile = function(e) {
+        var newProfileContainer = $('#base-container li').clone().
+            find('input').attr('data-originalkey', 'false').end().
+            find('.delete-profile').attr('data-originalkey', 'false').end().
+            wrap('<div/>').parent().html();
+        $('#edit-profile-list').append(newProfileContainer);
+
+        $('#edit-profiles-success').hide();
+        $('#save-profiles').attr("disabled", true).addClass('disabled-button');
+    };
+       
+    var saveProfiles = function() {
+        var nameVal,
+            regexVal;
+
+        localStorage.clear();
+        $('#edit-profile-list input').each(function(index,data) {
+            nameVal = $(this).val();
+            $(this).data('originalkey', nameVal);
+            regexVal =  $(this).parent().siblings('div').find('textarea').val();
+            localStorage.setItem(nameVal, regexVal);
+        });
+        $('#edit-profiles-success').show();
+        $('#save-profiles').attr("disabled", true).addClass('disabled-button');
     };
 
     var deleteProfile = function(e) {
-        var originalName = $(e.currentTarget).siblings('div').find('input').data('originalkey');
-        localStorage.removeItem(originalName);
+        var originalName = $(e.currentTarget).data('originalkey');
+
+        if(originalName !== 'false'){
+            localStorage.removeItem(originalName);
+        }
         $(e.currentTarget).parent().remove();
     };
-    var profileChanged = function(e) {
-        $('.save-profiles').attr("disabled", false).removeClass('disabledButton');
-    };
-
-    //event listeners
-    $('#pdf-container').on('dragover', dragOver);
-    $('#pdf-container').on('dragleave', dragLeave);
-    $('#pdf-container').on('drop', dropFile);
-    $('input[name="pdfFile"]').on('change', checkForm);
-    $('.profile-dropdown').on('change', checkForm);
-    $('.customProfileRegex').on('keyup', checkForm);
-    $('input[name="customProfile"]').on('keyup', checkForm);
-    $("form").submit(submitForm);
-    $(".save-profiles").on('click', saveProfiles);
-    
-
-
-    for (var key in localStorage) {
-        $('nav ul').append('<li><a href="/edit-profiles">Edit Custom Profiles</a></li>');
-        break;
-    }
-
-    if(window.location.pathname === '/') {
-        options = {
-
-            // Required. Called when a user selects an item in the Chooser.
-            success: function(files) {
-                console.log(files);
-                $('input[name="pdfFile"]').prop('files', files[0].link);
-            },
-
-            // Optional. Called when the user closes the dialog without selecting a file
-            // and does not include any parameters.
-            cancel: function() {
-
-            },
-
-            // Optional. "preview" (default) is a preview link to the document for sharing,
-            // "direct" is an expiring link to download the contents of the file. For more
-            // information about link types, see Link types below.
-            linkType: "direct", // or "direct"
-
-            // Optional. A value of false (default) limits selection to a single file, while
-            // true enables multiple file selection.
-            multiselect: false, // or true
-
-            // Optional. This is a list of file extensions. If specified, the user will
-            // only be able to select files with these extensions. You may also specify
-            // file types, such as "video" or "images" in the list. For more information,
-            // see File types below. By default, all extensions are allowed.
-            extensions: ['.pdf'],
-        };
-        // var button = Dropbox.createChooseButton(options);
-        // $('.dropbox-container').html(button);
-    }
-
-    if(window.location.pathname === '/edit-profiles') {
-        var $listContainer = $('.profile-list');
-        for (var key in localStorage) {
-            $listContainer.append(
-                "<li><div><input class='profile-name' data-originalkey='" + key + "' value=" + key + "/></div><div><textarea>" + localStorage[key] + "</textarea></div><span class='delete-profile'>&times;</span></li>"
-            );
-        }
-        $('.delete-profile').on('click', deleteProfile);
-        $('input').on('keyup', profileChanged);
-        $('textarea').on('keyup', profileChanged);
-    }
-
-});
+     
+}(window.jQuery, window, document));
